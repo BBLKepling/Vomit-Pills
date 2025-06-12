3e1a0d30a67fb8c608cb113391815080e6bef7c2
BBLKepling <90485361+BBLKepling@users.noreply.github.com>
2025-06-11 20:18:42 -0700

    1.6 update

---
+++ 1.6/Source/Vomit Pills/IngestionOutcomeDoer_Vomit.cs
@@ -0,0 +1,14 @@
+﻿using RimWorld;
+using Verse;
+
+namespace Vomit_Pills
+{
+    public class IngestionOutcomeDoer_Vomit : IngestionOutcomeDoer
+    {
+        protected override void DoIngestionOutcomeSpecial(Pawn pawn, Thing ingested, int ingestedCount)
+        {
+            pawn.jobs.StopAll();
+            pawn.jobs.StartJob(JobMaker.MakeJob(JobDefOf.Vomit));
+        }
+    }
+}
